b879fce7eeb1f0a33436fa4fb7b54caf6d4c35ae
Tomas Pavelka <info@tomaspavelka.cz>
2011-02-17 12:28:50 +0100

    Rescue for generating image

--- app/controllers/simple_captcha_controller.rb
+++ app/controllers/simple_captcha_controller.rb
@@ -5,11 +5,15 @@ class SimpleCaptchaController < ActionController::Metal
   # GET /simple_captcha
   def show
     unless params[:id].blank?
+      begin
       send_file(
         generate_simple_captcha_image(params[:id]),
         :type => 'image/jpeg',
         :disposition => 'inline',
         :filename => 'simple_captcha.jpg')
+      rescue StandardError
+        self.response_body = [404, {"Content-Type" => "text/html"}, ["Not Found"]]
+      end
     else
       self.response_body = [404, {"Content-Type" => "text/html"}, ["Not Found"]]
     end
